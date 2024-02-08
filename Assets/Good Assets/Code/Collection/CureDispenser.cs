a17e75536e7a77dffabce2b6b44a5e96613940a5
WUStudent <wustudent@webster.edu>
2024-02-07 20:12:38 -0600

    Final Push, Flower Enemys

--- Assets/Good Assets/Code/Collection/CureDispenser.cs
+++ Assets/Good Assets/Code/Collection/CureDispenser.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CureDispenser : MonoBehaviour
 {
     public GameManager gameManager;
+    public static GameObject Instance;
     private bool isPlayerInSpot = false;
     // Start is called before the first frame update
     void Start()
