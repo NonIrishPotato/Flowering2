fb239e2915ecff7b3a1c4ffec3ae8247587a171c
WUStudent <wustudent@webster.edu>
2024-02-14 20:36:55 -0600

    Begining of inventory

---
+++ Assets/Good Assets/Code/UI/InventorySystem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    /*
+     * 1 = Good Berrys
+     * 2 =
+     * 3 =
+     * ...
+     */
+    public int[] Items;
+    private int[] backpackLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
