5601add883e1642fc627f614a47127a2a77d7e9e
Rainbowzilla <xwingstarfighter301@gmail.com>
2024-02-21 17:02:27 -0800

    Added Pause Menu Script
    This is a placeholder for future UI. Players can press "ESCAPE" key to pause and resume game. There's a Resume Button, Return To Menu Button, and Quit Button. If there's a bug like sounds or animations playing. Please let me know and I'll try to fix the issue.

---
+++ Assets/PauseMenuScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void ReturnToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
