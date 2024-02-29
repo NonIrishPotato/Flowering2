c4b7b8c61ec8a3ee55a250cb006608e572afb60c
Rainbowzilla <xwingstarfighter301@gmail.com>
2024-02-28 18:02:52 -0800

    Made a Win Menu and Dialogue system
    To use the Dialogue system, add elements to add more lines. Then you can simply adjust the text speed for faster reading.

--- Assets/PauseMenuScript.cs
+++ Assets/PauseMenuScript.cs
@@ -7,20 +7,26 @@ public class PauseMenuScript : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameObject settingsMenu;
+    public GameObject gameOverMenu;
+    public GameObject winMenu;
     public static bool isPaused;
+    private bool didPlayerWin;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (!didPlayerWin)
         {
-            if (isPaused)
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                ResumeGame();
-            }
-            else
-            {
-                PauseGame();
+                if (isPaused)
+                {
+                    ResumeGame();
+                }
+                else
+                {
+                    PauseGame();
+                }
             }
         }
     }
@@ -40,7 +46,7 @@ public class PauseMenuScript : MonoBehaviour
         isPaused = false;
     }
 
-    public void SettingsMenu()
+    public void GoToSettingsMenu()
     {
         pauseMenu.SetActive(false);
         settingsMenu.SetActive(true);
@@ -52,10 +58,35 @@ public class PauseMenuScript : MonoBehaviour
         settingsMenu.SetActive(false);
     }
 
+    public void GameOver()
+    {
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        gameOverMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void YouWin()
+    {
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        winMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        didPlayerWin = true;
+    }
+
     public void ReturnToMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void QuitGame()
