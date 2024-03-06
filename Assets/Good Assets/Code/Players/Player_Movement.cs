f4d8c4e115210b687cab2a403ed9b5959bae55cb
MailMan805 <126529849+MailMan805@users.noreply.github.com>
2024-03-06 17:18:07 -0600

    Enemy Stuff

--- Assets/Good Assets/Code/Players/Player_Movement.cs
+++ Assets/Good Assets/Code/Players/Player_Movement.cs
@@ -131,7 +131,7 @@ public class Player_Movement : MonoBehaviour
     private void Jump()
     {
         //commented out grounded check just for the demo of sprint 2, need to fly around
-        if (/*isGrounded && */Input.GetButtonDown("Jump"))
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
