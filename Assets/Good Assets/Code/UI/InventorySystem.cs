7b7d56e42082bd4c77942bf9ed6ebd9cabe8461b
MailMan805 <126529849+MailMan805@users.noreply.github.com>
2024-03-20 18:38:43 -0500

    Yoink

--- Assets/Good Assets/Code/UI/InventorySystem.cs
+++ Assets/Good Assets/Code/UI/InventorySystem.cs
@@ -10,27 +10,44 @@ public class InventorySystem : MonoBehaviour
      * 0 = Empty Slot
      * 1 = Test Berry 1
      * 2 = Test Berry 2
-     * 3 = 
+     * 3 = Cure Berries
+     * 4 = Necterblooms
+     * 5 - Bloated fungus
+     * 6 = Molted Cicada Skins
+     * 7 = Rock
+     * 8 = Smoke Bomb
+     * 9 = Healing Honey
      * ...
      */
     private GameManager gameManager;
-    public Image[] InventorySlots; //All image place holders for inventory
+    public Image[] InventorySlots; //All image place holders for inventory and crafting slots
     public Image[] KeyItemSlots; //All image place holders for Key Items
-    public Image[] CraftingSlots;//All image place holders for Crafting Slots
     public Image[] ToolSlots;//All image place holders for Tools
+    public Image CraftedItemPlaceHolder;
     public int[] Items; //This represents all items in the inventory slots, there is a max of 22
     public int[] KeyItems; //This represents all key items
     public int[] ToolItems; //This represents all Tool Items
-    public int[] CraftingItems; //This represents all Crafted Items
+    public int[] CraftingSlots;
+    public int CrafterItemPlaceholderType = 0;
 
     public Sprite emptySlot;
     public Sprite testBerry;
     public Sprite testBerry2;
+    public Sprite CureBerry;
+    public Sprite Necterblooms;
+    public Sprite BloatedFungus;
+    public Sprite MoltedCicadaSkins;
+    public Sprite Rock;
+    public Sprite SmokeBomb;
+    public Sprite HealingHoney;
 
     private Image draggedItem;
     private int draggedItemIndex;
     private Image targetSlot;
     private int targetSlotIndex;
+
+
+    private int amountOfTestBerrys = 0;
 
     private RectTransform draggedItemTransform;
 
@@ -49,9 +66,6 @@ public class InventorySystem : MonoBehaviour
         //Checks Tool slots and fills them appropriately
         UpdateToolSlots();
 
-        //Checks Crafting Slots and fills them appropriately
-        UpdateCraftingSlots();
-
         //Count Berrys
         countBerrys();      
     }
@@ -69,9 +83,8 @@ public class InventorySystem : MonoBehaviour
         //Checks Tool slots and fills them appropriately
         UpdateToolSlots();
 
-        //Checks Crafting Slots and fills them appropriately
-        UpdateCraftingSlots();
-
+        //Updates What the Player can craft
+        UpdateCraftingPlaceholder();
 
         countBerrys();
     }
@@ -108,7 +121,8 @@ public class InventorySystem : MonoBehaviour
 
     private void UpdateInventorySlots()
     {
-        for (int i = 0; i < InventorySlots.Length; i++)
+
+        for (int i = 0; i < 22; i++)
         {
             if (Items[i] == 0)
             {
@@ -123,6 +137,24 @@ public class InventorySystem : MonoBehaviour
                 InventorySlots[i].sprite = testBerry2;
             }
         }
+
+        //For Crafting
+        for (int i = 22; i < InventorySlots.Length; i++)
+        {
+            if (Items[i] == 0)
+            {
+                InventorySlots[i].sprite = emptySlot;
+            }
+            else if (Items[i] == 1)
+            {
+                InventorySlots[i].sprite = testBerry;
+            }
+            else if (Items[i] == 2)
+            {
+                InventorySlots[i].sprite = testBerry2;
+            }
+        }
+
     }
 
     private void UpdateKeyItemSlots()
@@ -146,15 +178,97 @@ public class InventorySystem : MonoBehaviour
             }
         }
     }
-    private void UpdateCraftingSlots()
-    {
-        for (int i = 0; i < CraftingSlots.Length; i++)
-        {
-            if (CraftingItems[i] == 0)
-            {
-                CraftingSlots[i].sprite = emptySlot;
-            }
-        }
-    }
 
+    private void UpdateCraftingPlaceholder()
+    {
+        
+
+            //Getting the Crafting items in the boxes
+            if (CraftingSlots[0] == 0)
+            {
+                CraftingSlots[0] = Items[22];
+            }
+            //Debug.Log("Slot 1 = " + CraftingSlots[0]);
+            if (CraftingSlots[1] == 0)
+            {
+                CraftingSlots[1] = Items[23];
+            }
+            //Debug.Log("Slot 2 = " + CraftingSlots[1]);
+            if (CraftingSlots[2] == 0)
+            {
+                CraftingSlots[2] = Items[24];
+            }
+            //Debug.Log("Slot 3 = " + CraftingSlots[2]);
+            if (CraftingSlots[3] == 0)
+            {
+                CraftingSlots[3] = Items[25];
+            }
+            // Debug.Log("Slot 4 = " + CraftingSlots[3]);
+            if (CraftingSlots[4] == 0)
+            {
+                CraftingSlots[4] = Items[26];
+            }
+            //Debug.Log("Slot 5 = " + CraftingSlots[4]);
+            if (CraftingSlots[5] == 0)
+            {
+                CraftingSlots[5] = Items[27];
+            };
+            //Debug.Log("Slot 6 = " + CraftingSlots[5]);
+       
+
+
+        if (CraftingSlots[0] == 1)
+        {
+            amountOfTestBerrys += 1;
+        }
+        if (CraftingSlots[1] == 1)
+        {
+            amountOfTestBerrys += 1;
+        }
+        if (CraftingSlots[2] == 1)
+        {
+            amountOfTestBerrys += 1;
+        }
+        if (CraftingSlots[3] == 1)
+        {
+            amountOfTestBerrys += 1;
+        }
+        if (CraftingSlots[4] == 1)
+        {
+            amountOfTestBerrys += 1;
+        }
+        if (CraftingSlots[5] == 1)
+        {
+            amountOfTestBerrys += 1;
+        }
+
+
+
+        if (amountOfTestBerrys == 2)
+        {
+            CrafterItemPlaceholderType = 2;
+        }
+
+
+
+
+        if (CrafterItemPlaceholderType == 0)
+        {
+            CraftedItemPlaceHolder.sprite = emptySlot;
+        }
+        if(CrafterItemPlaceholderType == 2)
+        {
+            CraftedItemPlaceHolder.sprite = testBerry2;
+        }
+        if (CrafterItemPlaceholderType == 8)
+        {
+            CraftedItemPlaceHolder.sprite = SmokeBomb;
+        }
+        if (CrafterItemPlaceholderType == 9)
+        {
+            CraftedItemPlaceHolder.sprite = HealingHoney;
+        }
+
+        amountOfTestBerrys = 0;
+    }
 }
