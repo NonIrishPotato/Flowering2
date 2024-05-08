fc34d4ca1501b1d7f2278158f9f9ae23a919aa68
MailMan805 <126529849+MailMan805@users.noreply.github.com>
2024-05-08 14:23:06 -0500

    wsgrg

--- Assets/Good Assets/Code/Players/BackgroundFollow.cs
+++ Assets/Good Assets/Code/Players/BackgroundFollow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundFollow : MonoBehaviour
+public class BackgroundFolloww : MonoBehaviour
 {
     public Transform player; // Reference to the player's transform
     public Transform Background;
