8f1f70b4ef1a3f74e5ef0b006c78c3c0b40463ea
moshaeMO <161900780+Italianpianokey@users.noreply.github.com>
2025-04-23 18:55:10 -0500

    player movement works

--- Assets/Code/Players/Player_Movement.cs
+++ Assets/Code/Players/Player_Movement.cs
@@ -147,7 +147,6 @@ public class Player_Movement : MonoBehaviour
             RechargeFuel();
 
             // Check for damage
-            CheckForDamage();
 
             // Idle State
             IdleState();
